7a4ac23a72c84bbb99d0e225fe6aa5beb8d9d1d7
Sikanto Mandal <pogo591998@gmail.com>
2023-04-15 11:38:46 +0530

    Attributes Use

--- app/src/main/java/com/example/unuconverter/MainActivity.java
+++ app/src/main/java/com/example/unuconverter/MainActivity.java
@@ -19,18 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
-        editText= findViewById(R.id.editText);
+        editText = findViewById(R.id.editText);
         textView = findViewById(R.id.textView);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Thank You", Toast.LENGTH_SHORT).show();
-                String s =editText.getText().toString();
-                int kg = Integer.parseInt(s);
-                double pound = 2.205 * kg;
-                textView.setText("The corresponding value in Pounds is " + pound);
-            }
-        });
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Thank You", Toast.LENGTH_SHORT).show();
+//                String s =editText.getText().toString();
+//                int kg = Integer.parseInt(s);
+//                double pound = 2.205 * kg;
+//                textView.setText("The corresponding value in Pounds is " + pound);
+//            }
+//        });
+
+    }
+        public void calculate(View view){
+
+            Toast.makeText(MainActivity.this, "Thank You", Toast.LENGTH_SHORT).show();
+            String s =editText.getText().toString();
+            int kg = Integer.parseInt(s);
+            double pound = 2.205 * kg;
+            textView.setText("The corresponding value in Pounds is " + pound);
+
 
     }
 }
